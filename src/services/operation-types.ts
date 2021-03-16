1015fe6f217a0903385c2ba80079fcdaa23700c8
Paul.Benn-Darias <paul.benn-darias@bjss.com>
2021-03-16 17:07:51 +0000

    known SQL operation types

---
+++ src/services/operation-types.ts
@@ -0,0 +1,16 @@
+import {OperationType} from "aws-sdk/clients/dynamodbstreams";
+
+export type KnownOperationType = "INSERT" | "UPDATE" | "DELETE";
+
+export const parseOperationType = (operationType: OperationType): KnownOperationType => {
+    switch(operationType) {
+        case "INSERT":
+            return "INSERT"
+        case "MODIFY":
+            return "UPDATE"
+        case "REMOVE":
+            return "DELETE";
+        default:
+            throw new Error(`unrecognized operation type ${operationType}`)
+    }
+}
