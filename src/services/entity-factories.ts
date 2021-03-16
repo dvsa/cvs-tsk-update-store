6bc2c993f4ccfef6ba1ab049d91b81f4ad92f969
Paul.Benn-Darias <paul.benn-darias@bjss.com>
2021-03-16 17:15:26 +0000

    Entity factory (with no functions set)

---
+++ src/services/entity-factories.ts
@@ -0,0 +1,17 @@
+import {toTechnicalRecord} from "../models/technical-records";
+import {DynamoDbImage} from "./dynamodb-images";
+import {DatabaseEntity} from "./database-operations";
+
+export type EntityFactory = (image: DynamoDbImage) => DatabaseEntity;
+
+const entityFactories: Map<string, EntityFactory> = new Map();
+
+export const getEntityFactory = (tableName: string): EntityFactory => {
+    const entityFactory: ((image: DynamoDbImage) => DatabaseEntity) | undefined = entityFactories.get(tableName);
+
+    if (!entityFactory) {
+        throw new Error(`no entity factory for table '${tableName}'`);
+    }
+
+    return entityFactory;
+}
