7c9e5e2e192dfcf762d721b4a281dd331f354826
Paul.Benn-Darias <paul.benn-darias@bjss.com>
2021-03-23 17:42:54 +0000

    clean-up handler.ts

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,5 @@
 import {processStreamEvent} from "./functions/process-stream-event";
 import {config as AWSConfig} from "aws-sdk";
-import {DatabaseOperations} from "./services/database-operations";
 
 const isOffline: boolean = (!process.env.BRANCH || process.env.BRANCH === "local");
 
@@ -10,9 +9,5 @@ if (isOffline) {
         secretAccessKey: "verySecretKey1"
     };
 }
-
-// Share a repository layer: if the container is re-used, connection pool can be re-used across invocations
-// https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Streams.Lambda.BestPracticesWithDynamoDB.html
-export const DB_OPERATIONS = new DatabaseOperations();
 
 export {processStreamEvent as handler};
