1d12ab036359db07dd76515a9826088d45738976
Paul.Benn-Darias <paul.benn-darias@bjss.com>
2021-03-29 15:17:33 +0100

    Add example SELECT expectation

--- tests/integration/tech-record-conversion.intTest.ts
+++ tests/integration/tech-record-conversion.intTest.ts
@@ -1,7 +1,7 @@
 import {StartedTestContainer} from "testcontainers";
 import {getConnectionPoolConfiguration} from "../../src/services/database-configuration";
 import {PoolOptions} from "mysql2";
-import {destroyConnectionPool} from "../../src/services/connection-pool";
+import {destroyConnectionPool, execute} from "../../src/services/connection-pool";
 import {castToImageShape} from "../utils";
 import {convertTechRecordDocument} from "../../src/services/tech-record-conversion";
 import techRecordDocumentJson from "../resources/dynamodb-image-technical-record.json";
@@ -28,5 +28,10 @@ describe.skip("convertTechRecordDocument() integration tests", () => {
 
     it("should correctly convert a DynamoDB event into Aurora rows", async () => {
         await convertTechRecordDocument("INSERT", DynamoDbImage.parse(castToImageShape(techRecordDocumentJson)));
+
+        // TODO expand this SELECT to '*', check every row
+        const response = await execute("SELECT `system_number` FROM `vehicle` WHERE `vehicle`.`vin` = 'VIN'");
+
+        expect(response.rows[0].system_number).toEqual("SYSTEM-NUMBER");
     }, 1000000);
 });
