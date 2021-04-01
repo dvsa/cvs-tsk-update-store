9b674f012b5b49e9ac0fddeadb5ac2cca648dc7c
Paul.Benn-Darias <paul.benn-darias@bjss.com>
2021-04-01 11:10:21 +0100

    Check first property of every root and child in tech record document parsing test

--- tests/unit/models/tech-record-document.unitTest.ts
+++ tests/unit/models/tech-record-document.unitTest.ts
@@ -9,12 +9,20 @@ describe("parseTechRecordDocument()", () => {
 
         const techRecordDocument: TechRecordDocument = parseTechRecordDocument(image);
 
+        // check only first property of each root, for now
         expect(techRecordDocument.systemNumber).toEqual("SYSTEM-NUMBER");
-        expect(techRecordDocument.partialVin).toEqual("PARTIAL-VIN");
-        expect(techRecordDocument.primaryVrm).toEqual("999999999");
-        expect(techRecordDocument.secondaryVms).toEqual(["SECONDARY-VRM"]);
-        expect(techRecordDocument.vin).toEqual("VIN");
-        expect(techRecordDocument.trailerId).toEqual("88888888");
-        expect(techRecordDocument.techRecord!.length).toEqual(1);
+        expect(techRecordDocument.techRecord![0].recordCompleteness).toEqual("88888888");
+        expect(techRecordDocument.techRecord![0].authIntoService?.cocIssueDate).toEqual("2020-01-01");
+        expect(techRecordDocument.techRecord![0].lettersOfAuth?.letterType).toEqual("Trailer authorization");
+        expect(techRecordDocument.techRecord![0].applicantDetails?.name).toEqual("NAME");
+        expect(techRecordDocument.techRecord![0].purchaserDetails?.name).toEqual("NAME");
+        expect(techRecordDocument.techRecord![0].manufacturerDetails?.name).toEqual("NAME");
+        expect(techRecordDocument.techRecord![0].microfilm?.microfilmDocumentType).toEqual("PSV Miscellaneous");
+        expect(techRecordDocument.techRecord![0].plates![0].plateSerialNumber).toEqual("1");
+        expect(techRecordDocument.techRecord![0].bodyType?.code).toEqual("a");
+        expect(techRecordDocument.techRecord![0].dimensions?.axleSpacing![0].axles).toEqual("1-2");
+        expect(techRecordDocument.techRecord![0].vehicleClass?.code).toEqual("2");
+        expect(techRecordDocument.techRecord![0].brakes?.brakeCodeOriginal).toEqual("333");
+        expect(techRecordDocument.techRecord![0].axles![0].axleNumber).toEqual(1);
     });
 });
