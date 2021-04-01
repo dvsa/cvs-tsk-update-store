777c3ef1137cc113aacf7ab5e3313ce10ae2d543
Paul.Benn-Darias <paul.benn-darias@bjss.com>
2021-04-01 11:44:28 +0100

    Remove remnant RDS data parameter parsing form plates.ts

--- src/models/plates.ts
+++ src/models/plates.ts
@@ -1,6 +1,4 @@
 import {DynamoDbImage} from "../services/dynamodb-images";
-import {SqlParametersList} from "aws-sdk/clients/rdsdataservice";
-import {stringParam} from "../services/sql-parameter";
 
 export type Plates = Plate[];
 
@@ -40,17 +38,6 @@ export const parsePlates = (platesImage?: DynamoDbImage): Plates => {
     }
 
     return plates;
-};
-
-export const toPlateSqlParameters = (plate: Plate): SqlParametersList => {
-    const sqlParameters: SqlParametersList = [];
-
-    sqlParameters.push(stringParam("plateSerialNumber", plate.plateSerialNumber!));
-    sqlParameters.push(stringParam("plateIssueDate", plate.plateIssueDate!));
-    sqlParameters.push(stringParam("plateReasonForIssue", plate.plateReasonForIssue!));
-    sqlParameters.push(stringParam("plateIssuer", plate.plateIssuer!));
-
-    return sqlParameters;
 };
 
 // TODO add toPlateTemplateVariables
