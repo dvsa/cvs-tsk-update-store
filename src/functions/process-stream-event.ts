15c63079656f7a1b83d4afe4c6e354431d45342b
Paul.Benn-Darias <paul.benn-darias@bjss.com>
2021-04-07 14:11:51 +0100

    Clean up process-stream-event and add unit tests

--- src/functions/process-stream-event.ts
+++ src/functions/process-stream-event.ts
@@ -6,9 +6,9 @@ import {DynamoDbImage} from "../services/dynamodb-images";
 import {KnownOperationType, parseOperationType} from "../services/operation-types";
 
 /**
- * λ function to process an SQS message detailing info for update store
- * @param event - DynamoDB Stream event
- * @param context - λ Context
+ * λ function: convert a DynamoDB document to Aurora RDS rows
+ * @param event - DynamoDB stream event, containing DynamoDB document image
+ * @param context - λ context
  */
 export const processStreamEvent: Handler = async (event: DynamoDBStreamEvent, context: Context): Promise<any> => {
     validateEvent(event);
@@ -16,16 +16,23 @@ export const processStreamEvent: Handler = async (event: DynamoDBStreamEvent, co
     for await (const record of event.Records) {
         validateRecord(record);
 
+        // parse source ARN
         const eventSourceArn: EventSourceArn = stringToArn(record.eventSourceARN!);
+
+        // is this an INSERT, UPDATE, or DELETE?
         const operationType: KnownOperationType = parseOperationType(record.eventName!);
 
+        // parse native DynamoDB format to usable TS map
         const image: DynamoDbImage = selectImage(operationType, record.dynamodb!);
+
+        // use the source table name to figure out which conversion procedure we want to run
         const entityConverter: EntityConverter = getEntityConverter(eventSourceArn.table);
 
         try {
+            // perform conversion
             await entityConverter(operationType, image);
         } catch (e) {
-            console.error("couldn't apply entity converter", e);
+            console.error("couldn't convert DynamoDB entity to Aurora", e);
             dumpArguments(event, context);
         }
     }
@@ -35,8 +42,14 @@ const selectImage = (operationType: KnownOperationType, streamRecord: StreamReco
     switch (operationType) {
         case "INSERT":
         case "UPDATE":
+            if (!streamRecord.NewImage) {
+                throw new Error("'dynamodb' object missing required field 'NewImage'");
+            }
             return DynamoDbImage.parse(streamRecord.NewImage!);
         case "DELETE":
+            if (!streamRecord.OldImage) {
+                throw new Error("'dynamodb' object missing required field 'OldImage'");
+            }
             return DynamoDbImage.parse(streamRecord.OldImage!);
     }
 };
@@ -47,7 +60,7 @@ const validateEvent = (event: DynamoDBStreamEvent): void => {
     }
 
     if (!event.Records) {
-        throw new Error("missing required field event.Records");
+        throw new Error("event missing required field 'Records'");
     }
 
     if (!Array.isArray(event.Records)) {
@@ -56,6 +69,10 @@ const validateEvent = (event: DynamoDBStreamEvent): void => {
 };
 
 const validateRecord = (record: DynamoDBRecord): void => {
+    if (!record) {
+        throw new Error("record is null or undefined");
+    }
+
     if (!record.eventName) {
         throw new Error("record is missing required field \'eventName\'");
     }
---
+++ tests/unit/functions/process-stream-event.unitTest.ts
@@ -0,0 +1,164 @@
+import {processStreamEvent} from "../../../src/functions/process-stream-event";
+import {Context} from "aws-lambda";
+import {getEntityConverter} from "../../../src/services/entity-converters";
+import {KnownOperationType} from "../../../src/services/operation-types";
+import {DynamoDbImage} from "../../../src/services/dynamodb-images";
+
+describe("processStreamEvent()", () => {
+
+    it("should allow valid events to reach entity conversion procedure", async () => {
+        (getEntityConverter as jest.Mock) = jest.fn().mockReturnValueOnce(
+            (operationType: KnownOperationType, image: DynamoDbImage) => { return; }
+        );
+
+        await expect(processStreamEvent(
+            {
+                Records: [
+                    {
+                        eventName: "INSERT",
+                        dynamodb: {
+                            NewImage: {}
+                        },
+                        eventSourceARN: "arn:aws:dynamodb:eu-west-1:1:table/Technical_Records/stream/2020-01-01T00:00:00.000"
+                    }
+                ]
+            },
+            exampleContext(),
+            () => {
+                return;
+            })
+        ).resolves.not.toThrowError();
+    });
+
+    it("should fail on null event", async () => {
+        await expect(processStreamEvent(null, exampleContext(), () => {
+            return;
+        })).rejects.toThrowError();
+    });
+
+    it("should fail on event missing 'Records'", async () => {
+        await expect(processStreamEvent({}, exampleContext(), () => {
+            return;
+        })).rejects.toThrowError();
+    });
+
+    it("should fail on event where 'Records' is not an array", async () => {
+        await expect(processStreamEvent({Records: ""}, exampleContext(), () => {
+            return;
+        })).rejects.toThrowError();
+    });
+
+    it("should fail on null record", async () => {
+        await expect(processStreamEvent(
+            { Records: [ null ]},
+            exampleContext(),
+            () => {
+                return;
+            })
+        ).rejects.toThrowError();
+    });
+
+    it("should fail on record missing 'eventName'", async () => {
+        await expect(processStreamEvent(
+            { Records: [ {} ]},
+            exampleContext(),
+            () => {
+                return;
+            })
+        ).rejects.toThrowError();
+    });
+
+    it("should fail on record missing 'dynamodb'", async () => {
+        await expect(processStreamEvent(
+            {
+                Records: [
+                    {
+                        eventName: "INSERT"
+                    }
+                ]
+            },
+            exampleContext(),
+            () => {
+                return;
+            })
+        ).rejects.toThrowError();
+    });
+
+    it("should fail on record missing 'eventSourceARN'", async () => {
+        await expect(processStreamEvent(
+            {
+                Records: [
+                    {
+                        eventName: "INSERT",
+                        dynamodb: {}
+                    }
+                ]
+            },
+            exampleContext(),
+            () => {
+                return;
+            })
+        ).rejects.toThrowError();
+    });
+
+    it("should fail on record missing 'NewImage' when eventName is 'INSERT'", async () => {
+        await expect(processStreamEvent(
+            {
+                Records: [
+                    {
+                        eventName: "INSERT",
+                        dynamodb: {
+                            OldImage: {}
+                        },
+                        eventSourceARN: "arn:aws:dynamodb:eu-west-1:1:table/t/stream/2020-01-01T00:00:00.000"
+                    }
+                ]
+            },
+            exampleContext(),
+            () => {
+                return;
+            })
+        ).rejects.toThrowError();
+    });
+
+    it("should fail on record missing 'OldImage' when eventName is 'REMOVE'", async () => {
+        await expect(processStreamEvent(
+            {
+                Records: [
+                    {
+                        eventName: "REMOVE",
+                        dynamodb: {
+                            NewImage: {}
+                        },
+                        eventSourceARN: "arn:aws:dynamodb:eu-west-1:1:table/t/stream/2020-01-01T00:00:00.000"
+                    }
+                ]
+            },
+            exampleContext(),
+            () => {
+                return;
+            })
+        ).rejects.toThrowError();
+    });
+
+    const exampleContext = (): Context => {
+        return {
+            callbackWaitsForEmptyEventLoop: false,
+            functionName: "test",
+            functionVersion: "0.0.0",
+            invokedFunctionArn: "arn:aws:execute-api:eu-west-1:TEST",
+            memoryLimitInMB: "128",
+            awsRequestId: "TEST-AWS-REQUEST-ID",
+            logGroupName: "TEST-LOG-GROUP-NAME",
+            logStreamName: "TEST-LOG-STREAM-NAME",
+            getRemainingTimeInMillis: (): number => 86400000,
+            done: (): void => { /* circumvent TSLint no-empty */
+            },
+            fail: (): void => { /* circumvent TSLint no-empty */
+            },
+            succeed: (): void => { /* circumvent TSLint no-empty */
+            },
+        };
+    };
+});
+
