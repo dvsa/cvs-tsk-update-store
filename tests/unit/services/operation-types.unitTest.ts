1cb028d6311145ac22942e51822a34fa7375b8e4
Paul.Benn-Darias <paul.benn-darias@bjss.com>
2021-04-07 17:23:22 +0100

    Add unit tests for operation-types

---
+++ tests/unit/services/operation-types.unitTest.ts
@@ -0,0 +1,19 @@
+import {deriveSqlOperation} from "../../../src/services/operation-types";
+
+describe("deriveSqlOperation()", () => {
+    it("should return 'INSERT' for input 'INSERT'", () => {
+        expect(deriveSqlOperation("INSERT")).toEqual("INSERT");
+    });
+
+    it("should return 'UPDATE' for input 'MODIFY'", () => {
+        expect(deriveSqlOperation("MODIFY")).toEqual("UPDATE");
+    });
+
+    it("should return 'DELETE' for input 'REMOVE'", () => {
+        expect(deriveSqlOperation("REMOVE")).toEqual("DELETE");
+    });
+
+    it("should throw error on unrecognized operation", () => {
+        expect(() => deriveSqlOperation("any-unknown")).toThrowError();
+    });
+});
