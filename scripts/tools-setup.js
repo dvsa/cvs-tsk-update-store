45782d7d1a6e0e3846d9dd6bc390ce8bb908e423
Paul.Benn-Darias <paul.benn-darias@bjss.com>
2021-04-12 17:52:11 +0100

    tools-setup.js should use port 3306

--- scripts/tools-setup.js
+++ scripts/tools-setup.js
@@ -4,7 +4,7 @@ const path = require("path");
 (async () => await liquibase({
     liquibase: path.join(__dirname, "../node_modules/liquibase/lib/liquibase-4.0.0/liquibase"),
     changeLogFile: path.join(__dirname, "../cvs-nop/changelog-master.xml"),
-    url: 'jdbc:mysql://localhost:49169/CVSBNOP',
+    url: 'jdbc:mysql://localhost:3306/CVSBNOP',
     username: "root",
     password: "12345",
     classpath: path.join(__dirname, "../tests/resources/mysql-connector-java-8.0.23.jar")
