5701f13a274f82529d58d064e16e8013826fee88
Emanuel State <49722579+estatebjss@users.noreply.github.com>
2021-04-15 16:00:25 +0100

    Tests for techrecords and testresults (vott11 and vott54)

--- tests/integration/test-results-conversion.intTest.ts
+++ tests/integration/test-results-conversion.intTest.ts
@@ -67,79 +67,154 @@ describe("convertTestResults() integration tests", () => {
         const upsertResult = upsertResults[0][0];
 
         const vehicleResultSet = await executeSql(
-            `SELECT \`system_number\` FROM \`vehicle\` WHERE \`vehicle\`.\`id\` = ${upsertResult.vehicleId}`
+            `SELECT \`system_number\`, \`vin\`, \`vrm_trm\`, \`trailer_id\`, \`createdAt\`
+             FROM \`vehicle\`
+             WHERE \`vehicle\`.\`id\` = ${upsertResult.vehicleId}`
         );
         expect(vehicleResultSet.rows.length).toEqual(1);
         expect(vehicleResultSet.rows[0].system_number).toEqual("SYSTEM-NUMBER");
+        expect(vehicleResultSet.rows[0].vin).toEqual("VIN");
+        expect(vehicleResultSet.rows[0].vrm_trm).toEqual("VRM");
+        expect(vehicleResultSet.rows[0].trailer_id).toEqual("88888888");
+        expect((vehicleResultSet.rows[0].createdAt as Date).toUTCString()).not.toBeNull();
 
         const testStationResultSet = await executeSql(
-            `SELECT \`pNumber\` FROM \`test_station\` WHERE \`test_station\`.\`id\` = ${upsertResult.testStationId}`
+            `SELECT \`pNumber\`, \`name\`, \`type\`
+             FROM \`test_station\`
+             WHERE \`test_station\`.\`id\` = ${upsertResult.testStationId}`
         );
         expect(testStationResultSet.rows.length).toEqual(1);
         expect(testStationResultSet.rows[0].pNumber).toEqual("P-NUMBER");
+        expect(testStationResultSet.rows[0].name).toEqual("TEST-STATION-NAME");
+        expect(testStationResultSet.rows[0].type).toEqual("atf");
 
         const testerResultSet = await executeSql(
-            `SELECT \`staffId\` FROM \`tester\` WHERE \`tester\`.\`id\` = ${upsertResult.testerId}`
+            `SELECT \`staffId\`, \`name\`, \`email_address\`
+             FROM \`tester\`
+             WHERE \`tester\`.\`id\` = ${upsertResult.testerId}`
         );
         expect(testerResultSet.rows.length).toEqual(1);
         expect(testerResultSet.rows[0].staffId).toEqual("999999999");
+        expect(testerResultSet.rows[0].name).toEqual("TESTER-NAME");
+        expect(testerResultSet.rows[0].email_address).toEqual("TESTER-EMAIL-ADDRESS");
 
         const vehicleClassResultSet = await executeSql(
-            `SELECT \`code\` FROM \`vehicle_class\` WHERE \`vehicle_class\`.\`id\` = ${upsertResult.vehicleClassId}`
+            `SELECT \`code\`,
+                    \`description\`,
+                    \`vehicleType\`,
+                    \`vehicleSize\`,
+                    \`vehicleConfiguration\`,
+                    \`euVehicleCategory\`
+             FROM \`vehicle_class\`
+             WHERE \`vehicle_class\`.\`id\` = ${upsertResult.vehicleClassId}`
         );
         expect(vehicleClassResultSet.rows.length).toEqual(1);
         expect(vehicleClassResultSet.rows[0].code).toEqual("2");
+        expect(vehicleClassResultSet.rows[0].description).toEqual("motorbikes over 200cc or with a sidecar");
+        expect(vehicleClassResultSet.rows[0].vehicleType).toEqual("psv");
+        expect(vehicleClassResultSet.rows[0].vehicleSize).toEqual("large");
+        expect(vehicleClassResultSet.rows[0].vehicleConfiguration).toEqual("rigid");
+        expect(vehicleClassResultSet.rows[0].euVehicleCategory).toEqual("m1");
 
         const preparerResultSet = await executeSql(
-            `SELECT \`preparerId\` FROM \`preparer\` WHERE \`preparer\`.\`id\` = ${upsertResult.preparerId}`
+            `SELECT \`preparerId\`, \`name\`
+             FROM \`preparer\`
+             WHERE \`preparer\`.\`id\` = ${upsertResult.preparerId}`
         );
         expect(preparerResultSet.rows.length).toEqual(1);
         expect(preparerResultSet.rows[0].preparerId).toEqual("999999999");
+        expect(preparerResultSet.rows[0].name).toEqual("PREPARER-NAME");
 
         const createdByResultSet = await executeSql(
-            `SELECT \`identityId\` FROM \`identity\` WHERE \`identity\`.\`id\` = ${upsertResult.createdById}`
+            `SELECT \`identityId\`, \`name\`
+             FROM \`identity\`
+             WHERE \`identity\`.\`id\` = ${upsertResult.createdById}`
         );
         expect(createdByResultSet.rows.length).toEqual(1);
         expect(createdByResultSet.rows[0].identityId).toEqual("CREATED-BY-ID");
+        expect(createdByResultSet.rows[0].name).toEqual("CREATED-BY-NAME");
 
         const lastUpdatedByResultSet = await executeSql(
-            `SELECT \`identityId\` FROM \`identity\` WHERE \`identity\`.\`id\` = ${upsertResult.lastUpdatedById}`
+            `SELECT \`identityId\`, \`name\`
+             FROM \`identity\`
+             WHERE \`identity\`.\`id\` = ${upsertResult.lastUpdatedById}`
         );
         expect(lastUpdatedByResultSet.rows.length).toEqual(1);
         expect(lastUpdatedByResultSet.rows[0].identityId).toEqual("LAST-UPDATED-BY-ID");
+        expect(lastUpdatedByResultSet.rows[0].name).toEqual("LAST-UPDATED-BY-NAME");
 
         const fuelEmissionResultSet = await executeSql(
-            `SELECT \`modTypeCode\` FROM \`fuel_emission\` WHERE \`fuel_emission\`.\`id\` = ${upsertResult.fuelEmissionId}`
+            `SELECT \`modTypeCode\`, \`description\`, \`emissionStandard\`, \`fuelType\`
+             FROM \`fuel_emission\`
+             WHERE \`fuel_emission\`.\`id\` = ${upsertResult.fuelEmissionId}`
         );
         expect(fuelEmissionResultSet.rows.length).toEqual(1);
         expect(fuelEmissionResultSet.rows[0].modTypeCode).toEqual("p");
+        expect(fuelEmissionResultSet.rows[0].description).toEqual("particulate trap");
+        expect(fuelEmissionResultSet.rows[0].emissionStandard).toEqual("0.10 g/kWh Euro 3 PM");
+        expect(fuelEmissionResultSet.rows[0].fuelType).toEqual("diesel");
 
         const testTypeResultSet = await executeSql(
-            `SELECT \`testTypeName\`, \`testTypeClassification\` FROM \`test_type\` WHERE \`test_type\`.\`id\` = ${upsertResult.testTypeId}`
+            `SELECT \`testTypeClassification\`, \`testTypeName\`
+             FROM \`test_type\`
+             WHERE \`test_type\`.\`id\` = ${upsertResult.testTypeId}`
         );
         expect(testTypeResultSet.rows.length).toEqual(1);
         expect(testTypeResultSet.rows[0].testTypeClassification).toEqual("2323232323232323232323");
+        expect(testTypeResultSet.rows[0].testTypeName).toEqual("TEST-TYPE-NAME");
 
         expect(upsertResult.defectIds.length).toEqual(1);
+
         const defectResultSet = await executeSql(
-            `SELECT \`imNumber\` FROM \`defect\` WHERE \`defect\`.\`id\` = ${upsertResult.defectIds[0]}`
+            `SELECT \`imNumber\`,
+                    \`imDescription\`,
+                    \`itemNumber\`,
+                    \`itemDescription\`,
+                    \`deficiencyRef\`,
+                    \`deficiencyId\`,
+                    \`deficiencySubId\`,
+                    \`deficiencyCategory\`,
+                    \`deficiencyText\`,
+                    \`stdForProhibition\`
+             FROM \`defect\`
+             WHERE \`defect\`.\`id\` = ${upsertResult.defectIds[0]}`
         );
         expect(defectResultSet.rows.length).toEqual(1);
         expect(defectResultSet.rows[0].imNumber).toEqual(1);
+        expect(defectResultSet.rows[0].imDescription).toEqual("IM-DESCRIPTION");
+        expect(defectResultSet.rows[0].itemNumber).toEqual(1);
+        expect(defectResultSet.rows[0].itemDescription).toEqual("ITEM-DESCRIPTION");
+        expect(defectResultSet.rows[0].deficiencyRef).toEqual("DEFICIENCY-REF");
+        expect(defectResultSet.rows[0].deficiencyId).toEqual("a");
+        expect(defectResultSet.rows[0].deficiencySubId).toEqual("mdclxvi");
+        expect(defectResultSet.rows[0].deficiencyCategory).toEqual("advisory");
+        expect(defectResultSet.rows[0].deficiencyText).toEqual("DEFICIENCY-TEXT");
+        expect(defectResultSet.rows[0].itemNumber).toEqual(1);
 
         const testDefectResultSet = await executeSql(
-            `SELECT \`test_result_id\`, \`defect_id\` FROM \`test_defect\` WHERE \`test_defect\`.\`test_result_id\` = ${upsertResult.testResultId}`
+            `SELECT \`test_result_id\`, \`defect_id\`, \`location_id\`, \`notes\`, \`prs\`, \`prohibitionIssued\`
+             FROM \`test_defect\`
+             WHERE \`test_defect\`.\`test_result_id\` = ${upsertResult.testResultId}`
         );
         expect(testDefectResultSet.rows.length).toEqual(1);
         expect(testDefectResultSet.rows[0].test_result_id).toEqual(upsertResult.testResultId);
         expect(testDefectResultSet.rows[0].defect_id).toEqual(upsertResult.defectIds[0]);
+        expect(testDefectResultSet.rows[0].location_id).toEqual(1);
+        expect(testDefectResultSet.rows[0].notes).toEqual("NOTES");
+        expect(testDefectResultSet.rows[0].prs).toEqual(1);
+        expect(testDefectResultSet.rows[0].prohibitionIssued).toEqual(1);
 
         expect(upsertResult.customDefectIds.length).toEqual(1);
+
         const customDefectResultSet = await executeSql(
-            `SELECT \`test_result_id\`, \`referenceNumber\` FROM \`custom_defect\` WHERE \`custom_defect\`.\`id\` = ${upsertResult.customDefectIds[0]}`
+            `SELECT \`test_result_id\`, \`referenceNumber\`, \`defectName\`, \`defectNotes\`
+             FROM \`custom_defect\`
+             WHERE \`custom_defect\`.\`id\` = ${upsertResult.customDefectIds[0]}`
         );
         expect(customDefectResultSet.rows.length).toEqual(1);
         expect(customDefectResultSet.rows[0].test_result_id).toEqual(upsertResult.testResultId);
         expect(customDefectResultSet.rows[0].referenceNumber).toEqual("1010101010");
+        expect(customDefectResultSet.rows[0].defectName).toEqual("DEFECT-NAME");
+        expect(customDefectResultSet.rows[0].defectNotes).toEqual("DEFECT-NOTES");
     });
 });
