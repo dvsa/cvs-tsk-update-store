d5e148b16c2641c8a9bcf6ae8611d1eaf684a557
jonlazarini <jlazarini@deloitte.co.uk>
2021-05-25 17:56:15 +0100

    fix(cvsb-19400): fix vuln and add security-checks

--- src/handler.ts
+++ src/handler.ts
@@ -1,13 +1,14 @@
-import {processStreamEvent} from "./functions/process-stream-event";
-import {config as AWSConfig} from "aws-sdk";
+import { processStreamEvent } from "./functions/process-stream-event";
+import { config as AWSConfig } from "aws-sdk";
 
-const isOffline: boolean = (!process.env.BRANCH || process.env.BRANCH === "local");
+const isOffline: boolean =
+  !process.env.BRANCH || process.env.BRANCH === "local";
 
 if (isOffline) {
-    AWSConfig.credentials = {
-        accessKeyId: "accessKey1",
-        secretAccessKey: "verySecretKey1"
-    };
+  AWSConfig.credentials = {
+    accessKeyId: "accessKey1",
+    secretAccessKey: "verySecretKey1",
+  };
 }
 
-export {processStreamEvent as handler};
+export { processStreamEvent as handler };
