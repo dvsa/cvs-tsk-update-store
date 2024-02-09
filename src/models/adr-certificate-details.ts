847fd26805e4e9fce8f26680bc2b951a93a56fc7
Meys Torkaman <meys.torkaman@MacBook-Pro.local>
2024-02-09 09:20:38 +0000

    feat: cleaning and bringing in adr pass certificate

--- src/models/adr-certificate-details.ts
+++ src/models/adr-certificate-details.ts
@@ -1,49 +1,45 @@
 import { DynamoDbImage } from "../services/dynamodb-images";
 import { Maybe } from "./optionals";
 
-// define AdrCertificateDetails' high-level attributes data types
-export interface AdrCertificateDetails {
-    adrPassCertificateDetails?: AdrPassCertificateDetails;
-}
-
 // define Enums
-export type certificateIdEnum =
-  | "PASS"
-  | "REPLACEMENT";
+export type AdrPassCertificateTypeEnum = "PASS" | "REPLACEMENT";
 
-// define AdrCertificateDetails' sub-attributes data types
 export type AdrPassCertificateDetails = AdrPassCertificateDetailsItems[];
 
 export interface AdrPassCertificateDetailsItems {
-    createdByName?: string;
-    certificateType?: string;
-    generatedTimestamp?: string;
-    certificateId?: string;
+  createdByName?: string;
+  certificateType?: AdrPassCertificateTypeEnum;
+  generatedTimestamp?: string;
+  certificateId?: string;
+}
+
+export const parseAdrCertificateDetails = (
+  adrPassCertificateDetailsImage?: DynamoDbImage
+): Maybe<AdrPassCertificateDetails> => {
+  if (!adrPassCertificateDetailsImage) {
+    return undefined;
   }
 
-// function to parse AdrCertificateDetails' high-level and sub attributes + return AdrCertificateDetails object
-export const parseAdrCertificateDetails = (
-    certificateDetails?: DynamoDbImage
-  ): Maybe<AdrCertificateDetails> => {
-    if (!certificateDetails) {
-      return undefined;
-    }
+  const adrPassCertificateDetails: AdrPassCertificateDetails = [];
 
-    const adrPassCertificateDetailsImage: DynamoDbImage = certificateDetails.getList(
-        "adrPassCertificateDetails"
+  for (const key of adrPassCertificateDetailsImage.getKeys()) {
+    const adrPassCertificateDetailsItemImage = adrPassCertificateDetailsImage.getMap(
+      key
     )!;
-    const adrPassCertificateDetails: AdrPassCertificateDetails = [];
-
-    for (const key of adrPassCertificateDetailsImage.getKeys()) {
-        const adrPassCertificateDetailsItemImage = adrPassCertificateDetailsImage.getMap(key)!;
-        adrPassCertificateDetails.push({
-        createdByName: adrPassCertificateDetailsItemImage.getString("createdByName"),
-        certificateType: adrPassCertificateDetailsItemImage.getString("certificateType"),
-        generatedTimestamp: adrPassCertificateDetailsItemImage.getString("generatedTimestamp"),
-        certificateId: adrPassCertificateDetailsItemImage.getString("certificateId") as certificateIdEnum,
-        });
-    }
-    return {
-      adrPassCertificateDetails
-    };
+    adrPassCertificateDetails.push({
+      createdByName: adrPassCertificateDetailsItemImage.getString(
+        "createdByName"
+      ),
+      certificateType: adrPassCertificateDetailsItemImage.getString(
+        "certificateType"
+      ) as AdrPassCertificateTypeEnum,
+      generatedTimestamp: adrPassCertificateDetailsItemImage.getString(
+        "generatedTimestamp"
+      ),
+      certificateId: adrPassCertificateDetailsItemImage.getString(
+        "certificateId"
+      ),
+    });
+  }
+  return adrPassCertificateDetails;
 };
