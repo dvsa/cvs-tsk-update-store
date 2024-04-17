594f0b1f7524d20009ebbed5b8dfab6c81081d70
Furkan Erdogan <furkan.erdogan@bjss.com>
2024-04-17 13:34:20 +0100

    test: add jest-serial-runner to run sequentially

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,32 @@
 process.env.TZ = "GMT";
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/src", "<rootDir>/tests"],
-  setupFiles: ["jest-plugin-context/setup"],
-  moduleFileExtensions: ["js", "ts"],
-  testResultsProcessor: "jest-sonar-reporter",
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  testMatch: ["**/*.*Test.ts"],
-};
+  projects: [
+    {
+      displayName: "UNIT",
+      testMatch: ["<rootDir>/tests/**/*.unitTest.ts"],
+      preset: "ts-jest",
+      testEnvironment: "node",
+      roots: ["<rootDir>/tests"],
+      setupFiles: ["jest-plugin-context/setup"],
+      moduleFileExtensions: ["js", "ts"],
+      testResultsProcessor: "jest-sonar-reporter",
+      transform: {
+        "^.+\\.tsx?$": "ts-jest",
+      },
+    },
+    {
+      displayName: "INTEGRATION",
+      testMatch: ["<rootDir>/tests/**/*.intTest.ts"],
+      preset: "ts-jest",
+      testEnvironment: "node",
+      roots: ["<rootDir>/tests"],
+      setupFiles: ["jest-plugin-context/setup"],
+      moduleFileExtensions: ["js", "ts"],
+      testResultsProcessor: "jest-sonar-reporter",
+      transform: {
+        "^.+\\.tsx?$": "ts-jest",
+      },
+      runner: "jest-serial-runner",
+    }
+    ],
+  };
